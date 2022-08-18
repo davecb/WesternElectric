59c815344f664a91b10c854b7556299f663e0fd9
David Collier-Brown <dave.collier-brown@indexexchange.com>
2022-08-18 13:15:06 -0400

    Read some data

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 These are a classic set of decision rules in statistical process control for detecting out-of-control or non-random conditions.
 
-The book is available at https://dokumen.tips/download/link/western-electrics-statistical-quality-control-handbook
+The book is available at https://dokumen.tips/download/link/western-electrics-statistical-quality-control-handbook and a copy is in the doc directory.
 
 It is summarized at https://en.wikipedia.org/wiki/Western_Electric_rules
--- cmd/WesternElectric.go
+++ cmd/WesternElectric.go
@@ -1,21 +1,66 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+)
+
 /*
  * Western Electric Rules -- a classic set of decision rules in statistical process control
  * for detecting out-of-control or non-random conditions.
  */
 
-func main() {
-	// get number of samples, the tuning parameter
-	// create a worker
-	// run it
+func usage() {
+	//nolint
+	fmt.Fprint(os.Stderr, "Usage: westernelectrictc --samples N {file|-}\n")
+	flag.PrintDefaults()
+	os.Exit(1)
 }
 
-func worker() {
+func main() {
+	var samples int
+	var fp *os.File
+
+	flag.IntVar(&samples, "samples", 5, "number of samples in the moving average")
+	flag.Parse()
+
+	if flag.NArg() < 1 {
+		fmt.Fprint(os.Stderr, "You must supply an input file or '-' and a stream on stdin\n\n") //nolint
+		usage()
+	}
+	log.SetFlags(log.Lshortfile | log.Ldate | log.Ltime) // show file:line in logs
+
+	filename := flag.Arg(0)
+	if filename == "-" {
+		// if the filename is "-", read stdin
+		fp = os.Stdin
+	} else {
+		f, err := os.Open(filename) //nolint
+		if err != nil {
+			log.Fatalf("error opening %s: %q, halting.", filename, err)
+		}
+		defer func() {
+			err := f.Close()
+			if err != nil {
+				log.Printf("Close of r/o input file %q failed, ignored. %v",
+					filename, err)
+			}
+		}()
+	}
+	worker(fp, samples)
+	// create a worker
+	// run it
+	return
+}
+
+func worker(f *os.File, samples int) {
 	// while read stdin {
 	//		add
 	//		test
 	// 		print stats
 	// }
+	return
 
 }
