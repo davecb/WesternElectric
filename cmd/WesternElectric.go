4ee2619df6f94a8c0259f7bceeb8d818785f853d
David Collier-Brown <dave.collier-brown@indexexchange.com>
2022-08-19 16:20:52 -0400

    Parse lines, report average and sd

--- cmd/WesternElectric.go
+++ cmd/WesternElectric.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"encoding/csv"
 	"flag"
 	"fmt"
+	movingAverage "github.com/davecb/WesternElectric/pkg/MovingAverage"
+	"io"
 	"log"
 	"os"
+	"strconv"
 )
 
 /*
@@ -20,10 +24,11 @@ func usage() {
 }
 
 func main() {
-	var samples int
+	var nSamples int
 	var fp *os.File
+	var err error
 
-	flag.IntVar(&samples, "samples", 5, "number of samples in the moving average")
+	flag.IntVar(&nSamples, "nSamples", 5, "number of nSamples in the moving average")
 	flag.Parse()
 
 	if flag.NArg() < 1 {
@@ -37,30 +42,53 @@ func main() {
 		// if the filename is "-", read stdin
 		fp = os.Stdin
 	} else {
-		f, err := os.Open(filename) //nolint
+		fp, err = os.Open(filename) //nolint
 		if err != nil {
 			log.Fatalf("error opening %s: %q, halting.", filename, err)
 		}
 		defer func() {
-			err := f.Close()
+			err := fp.Close()
 			if err != nil {
-				log.Printf("Close of r/o input file %q failed, ignored. %v",
+				log.Printf("Close of input file %q failed, ignored. %v\n",
 					filename, err)
 			}
 		}()
 	}
-	worker(fp, samples)
-	// create a worker
-	// run it
-	return
+	worker(fp, nSamples)
 }
 
-func worker(f *os.File, samples int) {
-	// while read stdin {
-	//		add
-	//		test
-	// 		print stats
-	// }
-	return
+func worker(fp *os.File, nSamples int) {
+	var nr int
+	var average float64
+	var sd float64
 
+	// set up csv reader to read fields out of a file
+	r := csv.NewReader(fp)
+	r.Comma = '\t'
+	r.Comment = '#'
+	r.FieldsPerRecord = -1 // ignore differences
+	r.LazyQuotes = true    // allow bad quoting
+
+	// set up moving average
+	add := movingAverage.CreateMovingAverage(nSamples)
+
+	// read lines containing a data and a (usually floating-point) value
+	for nr = 0; ; nr++ {
+		record, err := r.Read()
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			log.Fatalf("error %q, in %q, line %d\n", err, record, nr)
+		}
+		datum, err := strconv.ParseFloat(record[1], 64)
+		if err != nil {
+			log.Fatalf("Encountered invalid float64 in %q, one line %d, halting.\n", record, nr)
+		}
+		average, sd = add(datum)
+		log.Printf("got %f, average = %f, sd = %f\n", datum, average, sd)
+		// 	add
+		//  test
+		// 	print stats
+	}
+	log.Printf("out of loop at line %d\n", nr)
 }
